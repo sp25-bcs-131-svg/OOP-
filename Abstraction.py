4f92ef668e5a64aa89323d9dba0abfabb381dbf0
Talha Munir <SP25-BCS-131@cuilahore.edu.pk>
2026-09-20 16:21:02 +0500

    changes 3

---
+++ Abstraction.py
@@ -0,0 +1,14 @@
+class Car():
+
+    def __init__(self):
+        self.acc = False
+        self.brk = False
+        self.clutch = False
+
+    def start(self):
+        self.clutch = True
+        self.acc = True
+        print("car started")
+
+c1 = Car()
+c1.start()
