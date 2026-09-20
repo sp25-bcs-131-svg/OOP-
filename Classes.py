2ee4179f45b9b10fcdbaa0741a71f8512c59eafa
Talha Munir <SP25-BCS-131@cuilahore.edu.pk>
2026-09-20 15:28:02 +0500

    changes 1

--- Classes.py
+++ Classes.py
@@ -1,15 +1,36 @@
 #Class
 
 class Student():
+
+    #Class Attribute
+    college_name = "Comsats"
+
+    #Default Constructors
+    def __init__(self, name, bases, dict, /, **kwds):
+        pass
+
+    #Parameterized constructors
     def __init__(self,name,marks):
+        #Instance Attribute
         self.name = name
         self.marks = marks
-        print("Creating a new student")
+
+    def hello(self):
+        print("hello", self.name)
+
+    def get_marks(self):
+        return self.marks
+    
+    
 
 s1 = Student("Umama",91)
 print(s1.name)
-print(s1.marks)
+print(s1.get_marks())
+print(s1.college_name)
+s1.hello()
 
 s2 = Student("Talha",82)
 print(s2.name)  
 print(s1.marks)
+
+print(Student.college_name)
