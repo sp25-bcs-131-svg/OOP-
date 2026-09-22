03dc701e4b3047a4108ee3e5f9f135deb7e08a51
Talha Munir <SP25-BCS-131@cuilahore.edu.pk>
2026-09-23 00:55:53 +0500

    changes 9

--- Private.py
+++ Private.py
@@ -9,3 +9,17 @@ class Account():
 c1 = Account('PK4045')
 #print(c1.__acc_no) causes error
 print(c1.get_acc())
+
+class Person():
+
+    __name = "Talha" # Private Attribute
+
+    def __hello(self): # Private Method
+        print("hello,",self.__name)
+
+    def welcome(self):
+        self.__hello()
+
+p1 = Person()
+#p1.__hello()
+print(p1.welcome())
