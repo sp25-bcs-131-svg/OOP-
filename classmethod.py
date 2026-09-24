e386e603f47e37a7f0e9dd682b925957f7cf7805
Talha Munir <SP25-BCS-131@cuilahore.edu.pk>
2026-09-24 23:38:11 +0500

    changes 14

--- classmethod.py
+++ classmethod.py
@@ -3,9 +3,16 @@ class Person():
     name = "anonymous"
 
     def change_name(self,name):
-        self.name = name
+        self.__class__.name = name
+        #Alternative solutions
+        #Person.name = name
+    
+    #Alternative solutions
+    @classmethod
+    def change_name2(cls,name):
+        cls.name =name
 
 p1 = Person()
-p1.change_name("Ahmad")
+p1.change_name2("Ahmad")
 print(p1.name)
 print(Person.name)
